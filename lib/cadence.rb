c279df5447554b91fb18cb7967f1c3a699d869f2
carlcortright <carl.cortright@coinbase.com>
2020-09-07 13:18:08 -0700

    add schedule workflow

--- lib/cadence.rb
+++ lib/cadence.rb
@@ -27,8 +27,30 @@ module Cadence
         execution_timeout: execution_options.timeouts[:execution],
         task_timeout: execution_options.timeouts[:task],
         workflow_id_reuse_policy: options[:workflow_id_reuse_policy],
+        headers: execution_options.headers
+      )
+
+      response.runId
+    end
+
+    def schedule_workflow(workflow, cron_schedule, *input, **args)
+      options = args.delete(:options) || {}
+      input << args unless args.empty?
+
+      execution_options = ExecutionOptions.new(workflow, options)
+      workflow_id = options[:workflow_id] || SecureRandom.uuid
+
+      response = client.start_workflow_execution(
+        domain: execution_options.domain,
+        workflow_id: workflow_id,
+        workflow_name: execution_options.name,
+        task_list: execution_options.task_list,
+        input: input,
+        execution_timeout: execution_options.timeouts[:execution],
+        task_timeout: execution_options.timeouts[:task],
+        workflow_id_reuse_policy: options[:workflow_id_reuse_policy],
         headers: execution_options.headers,
-        cron_schedule: execution_options.cron_schedule
+        cron_schedule: cron_schedule
       )
 
       response.runId
