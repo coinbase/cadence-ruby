67c69b97bc69608f162c1642073d1f7913a2d6f8
Jordan Nolan <jordanwn@gmail.com>
2021-01-06 13:07:33 -0500

    More tests and updates readme

--- spec/unit/lib/cadence/fiber_with_parent_locals_spec.rb
+++ spec/unit/lib/cadence/fiber_with_parent_locals_spec.rb
@@ -6,18 +6,54 @@ describe Cadence::FiberWithParentLocals do
   after { Thread.current[:test_key] = nil }
 
   describe '.new' do
-    let(:test_value) { { test: :hash } }
-    before { Thread.current[:test_key] = test_value }
 
-    it 'copies and does not modify parent thread variables' do
-      test_var = nil
-      subject.new do
-        test_var = Thread.current[:test_key]
-        test_var[:did_clone] = :cloned
-      end.resume
+    context 'it copies and clones' do
+      before { Thread.current[:test_key] = :test_value }
 
-      expect(test_value).to(eq({ test: :hash }))
-      expect(test_var).to(eq({ did_clone: :cloned, test: :hash }))
+      it 'it copies and clones' do
+        test_var = nil
+        subject.new do
+          test_var = Thread.current[:test_key]
+        end.resume
+
+        expect(test_var).to eq(:test_value)
+      end
     end
+
+    context 'modifying parent variables' do
+      let(:test_value) { { test: :value } }
+      before do
+        allow(test_value).to(receive(:clone).and_call_original)
+        Thread.current[:test_key] = test_value
+      end
+
+      it 'copies parent thread variables' do
+        test_var = nil
+        subject.new do
+          test_var = Thread.current[:test_key]
+          test_var[:did_clone] = :cloned
+        end.resume
+
+        expect(test_value).to(eq({ test: :value }))
+        expect(test_var).to(include({ test: :value, did_clone: :cloned}))
+      end
+
+      it 'calls clone on parent variables' do
+        subject.new {}.resume
+
+        expect(test_value).to(have_received(:clone))
+      end
+
+      it 'holds a different object id from parent variable' do
+        test_id = nil
+        subject.new do
+          test_id = Thread.current[:test_key].object_id
+        end.resume
+
+        expect(test_id).not_to(eq(test_value.object_id))
+      end
+    end
+
+
   end
 end
