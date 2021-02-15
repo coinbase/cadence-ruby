60066c868deab62769692178fe8ea15aa022c12d
Anthony Dmitriyev <antstorm@gmail.com>
2021-02-15 14:16:57 +0000

    [Feature] Report time between poller loops (#38)
    * Report time between activity polls
    
    * Report time between decision task polls
    
    * Freeze metric tags to avoid surprises

--- spec/unit/lib/cadence/workflow/poller_spec.rb
+++ spec/unit/lib/cadence/workflow/poller_spec.rb
@@ -1,14 +1,11 @@
-require 'cadence/activity/poller'
+require 'cadence/workflow/poller'
 require 'cadence/middleware/entry'
 
-describe Cadence::Activity::Poller do
+describe Cadence::Workflow::Poller do
   let(:client) { instance_double('Cadence::Client::ThriftClient') }
   let(:domain) { 'test-domain' }
   let(:task_list) { 'test-task-list' }
   let(:lookup) { instance_double('Cadence::ExecutableLookup') }
-  let(:thread_pool) do
-    instance_double(Cadence::ThreadPool, wait_for_available_threads: nil, shutdown: nil)
-  end
   let(:middleware_chain) { instance_double(Cadence::Middleware::Chain) }
   let(:middleware) { [] }
 
@@ -16,15 +13,14 @@ describe Cadence::Activity::Poller do
 
   before do
     allow(Cadence::Client).to receive(:generate).and_return(client)
-    allow(Cadence::ThreadPool).to receive(:new).and_return(thread_pool)
     allow(Cadence::Middleware::Chain).to receive(:new).and_return(middleware_chain)
-    allow(client).to receive(:poll_for_activity_task).and_return(nil)
     allow(Cadence.metrics).to receive(:timing)
   end
 
   describe '#start' do
-    it 'polls for activity tasks' do
+    it 'polls for decision tasks' do
       allow(subject).to receive(:shutting_down?).and_return(false, false, true)
+      allow(client).to receive(:poll_for_decision_task).and_return(nil)
 
       subject.start
 
@@ -32,14 +28,14 @@ describe Cadence::Activity::Poller do
       subject.stop; subject.wait
 
       expect(client)
-        .to have_received(:poll_for_activity_task)
+        .to have_received(:poll_for_decision_task)
         .with(domain: domain, task_list: task_list)
         .twice
     end
 
-    it 'polls for activity tasks' do
+    it 'polls for decision tasks' do
       allow(subject).to receive(:shutting_down?).and_return(false, false, true)
-      allow(client).to receive(:poll_for_activity_task).and_return(nil)
+      allow(client).to receive(:poll_for_decision_task).and_return(nil)
 
       subject.start
 
@@ -49,7 +45,7 @@ describe Cadence::Activity::Poller do
       expect(Cadence.metrics)
         .to have_received(:timing)
         .with(
-          'activity_poller.time_since_last_poll',
+          'workflow_poller.time_since_last_poll',
           an_instance_of(Fixnum),
           domain: domain,
           task_list: task_list
@@ -57,60 +53,25 @@ describe Cadence::Activity::Poller do
         .twice
     end
 
-    context 'with options passed' do
-      subject { described_class.new(domain, task_list, lookup, middleware, options) }
-      let(:options) { { polling_ttl: 42, thread_pool_size: 42 } }
+    context 'when an decision task is received' do
+      let(:task_processor) do
+        instance_double(Cadence::Workflow::DecisionTaskProcessor, process: nil)
+      end
+      let(:task) { Fabricate(:decision_task_thrift) }
 
       before do
         allow(subject).to receive(:shutting_down?).and_return(false, true)
+        allow(client).to receive(:poll_for_decision_task).and_return(task)
+        allow(Cadence::Workflow::DecisionTaskProcessor).to receive(:new).and_return(task_processor)
       end
 
-      it 'passes options to the client' do
+      it 'uses DecisionTaskProcessor to process tasks' do
         subject.start
 
         # stop poller before inspecting
         subject.stop; subject.wait
 
-        expect(Cadence::Client).to have_received(:generate).with(options)
-      end
-
-      it 'creates thread pool of a specified size' do
-        subject.start
-
-        # stop poller before inspecting
-        subject.stop; subject.wait
-
-        expect(Cadence::ThreadPool).to have_received(:new).with(42)
-      end
-    end
-
-    context 'when an activity task is received' do
-      let(:task_processor) { instance_double(Cadence::Activity::TaskProcessor, process: nil) }
-      let(:task) { Fabricate(:activity_task_thrift) }
-
-      before do
-        allow(subject).to receive(:shutting_down?).and_return(false, true)
-        allow(client).to receive(:poll_for_activity_task).and_return(task)
-        allow(Cadence::Activity::TaskProcessor).to receive(:new).and_return(task_processor)
-        allow(thread_pool).to receive(:schedule).and_yield
-      end
-
-      it 'schedules task processing using a ThreadPool' do
-        subject.start
-
-        # stop poller before inspecting
-        subject.stop; subject.wait
-
-        expect(thread_pool).to have_received(:schedule)
-      end
-
-      it 'uses TaskProcessor to process tasks' do
-        subject.start
-
-        # stop poller before inspecting
-        subject.stop; subject.wait
-
-        expect(Cadence::Activity::TaskProcessor)
+        expect(Cadence::Workflow::DecisionTaskProcessor)
           .to have_received(:new)
           .with(task, domain, lookup, client, middleware_chain)
         expect(task_processor).to have_received(:process)
@@ -126,14 +87,14 @@ describe Cadence::Activity::Poller do
         let(:entry_1) { Cadence::Middleware::Entry.new(TestPollerMiddleware, '1') }
         let(:entry_2) { Cadence::Middleware::Entry.new(TestPollerMiddleware, '2') }
 
-        it 'initializes middleware chain and passes it down to TaskProcessor' do
+        it 'initializes middleware chain and passes it down to DecisionTaskProcessor' do
           subject.start
 
           # stop poller before inspecting
           subject.stop; subject.wait
 
           expect(Cadence::Middleware::Chain).to have_received(:new).with(middleware)
-          expect(Cadence::Activity::TaskProcessor)
+          expect(Cadence::Workflow::DecisionTaskProcessor)
             .to have_received(:new)
             .with(task, domain, lookup, client, middleware_chain)
         end
@@ -143,7 +104,7 @@ describe Cadence::Activity::Poller do
     context 'when client is unable to poll' do
       before do
         allow(subject).to receive(:shutting_down?).and_return(false, true)
-        allow(client).to receive(:poll_for_activity_task).and_raise(StandardError)
+        allow(client).to receive(:poll_for_decision_task).and_raise(StandardError)
       end
 
       it 'logs' do
@@ -156,21 +117,8 @@ describe Cadence::Activity::Poller do
 
         expect(Cadence.logger)
           .to have_received(:error)
-          .with('Unable to poll for an activity task: #<StandardError: StandardError>')
+          .with('Unable to poll for a decision task: #<StandardError: StandardError>')
       end
-    end
-  end
-
-  describe '#wait' do
-    before do
-      subject.start
-      subject.stop
-    end
-
-    it 'shuts down the thread poll' do
-      subject.wait
-
-      expect(thread_pool).to have_received(:shutdown)
     end
   end
 end
