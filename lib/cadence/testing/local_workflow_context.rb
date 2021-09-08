9d31afa55973d2e670b6ecf41aff88144a47084d
Evelyn Florentine <evelyn.florentine@coinbase.com>
2021-09-08 09:26:16 -0400

    return future, updated indent

--- lib/cadence/testing/local_workflow_context.rb
+++ lib/cadence/testing/local_workflow_context.rb
@@ -61,12 +61,12 @@ module Cadence
         )
         context = LocalActivityContext.new(metadata)
 
-      begin
-        result = activity_class.execute_in_context(context, input)
-      rescue StandardError, ScriptError => error
-        future.fail(error.class.name, error.message)
-        return
-      end
+        begin
+          result = activity_class.execute_in_context(context, input)
+        rescue StandardError, ScriptError => error
+          future.fail(error.class.name, error.message)
+          return future
+        end
 
         if context.async?
           execution.register_future(activity_id, future)
