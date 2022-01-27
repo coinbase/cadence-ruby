c97771fce93ba8f8b551177973c7409fcc121fcd
Anthony Dmitriyev <antstorm@gmail.com>
2022-01-27 13:55:13 +0000

    fixup! Implement a versioned workflow concern

--- examples/spec/helpers.rb
+++ examples/spec/helpers.rb
@@ -1,4 +1,5 @@
 require 'securerandom'
+require 'cadence/workflow/history'
 
 module Helpers
   def run_workflow(workflow, *input, **args)
@@ -12,7 +13,7 @@ module Helpers
     client = Cadence.send(:default_client)
     connection = client.send(:connection)
 
-    connection.get_workflow_execution_history(
+    result = connection.get_workflow_execution_history(
       domain: Cadence.configuration.domain,
       workflow_id: workflow_id,
       run_id: run_id,
@@ -20,5 +21,7 @@ module Helpers
       wait_for_new_event: true,
       event_type: :close
     )
+
+    Cadence::History.new(result.history.events)
   end
 end
--- examples/spec/integration/versioned_workflow_spec.rb
+++ examples/spec/integration/versioned_workflow_spec.rb
@@ -5,10 +5,10 @@ describe VersionedWorkflow, :integration do
     it 'executes latest version' do
       result = run_workflow(described_class)
 
-      event = result.history.events.first
+      event = result.events.first
 
-      expect(event.eventType).to eq(CadenceThrift::EventType::WorkflowExecutionCompleted)
-      expect(event.result).to eq('ECHO: version 2')
+      expect(event.type).to eq(CadenceThrift::EventType::WorkflowExecutionCompleted)
+      expect(event.attributes.result).to eq('ECHO: version 2')
     end
   end
 
@@ -16,10 +16,10 @@ describe VersionedWorkflow, :integration do
     it 'executes specified version' do
       result = run_workflow(described_class, options: { headers: { 'Version' => '1' }})
 
-      event = result.history.events.first
+      event = result.events.first
 
-      expect(event.eventType).to eq(CadenceThrift::EventType::WorkflowExecutionCompleted)
-      expect(event.result).to eq('ECHO: version 1')
+      expect(event.type).to eq(CadenceThrift::EventType::WorkflowExecutionCompleted)
+      expect(event.attributes.result).to eq('ECHO: version 1')
     end
   end
 
@@ -27,10 +27,10 @@ describe VersionedWorkflow, :integration do
     it 'executes default version' do
       result = run_workflow(described_class, options: { headers: { 'Version' => nil }})
 
-      event = result.history.events.first
+      event = result.events.first
 
-      expect(event.eventType).to eq(CadenceThrift::EventType::WorkflowExecutionCompleted)
-      expect(event.result).to eq('ECHO: default version')
+      expect(event.type).to eq(CadenceThrift::EventType::WorkflowExecutionCompleted)
+      expect(event.attributes.result).to eq('ECHO: default version')
     end
   end
 end
