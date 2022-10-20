b87c96f1998d14474e3b79c25c26636a8e41b3b9
Francis Li <francis.li@coinbase.com>
2022-10-20 16:29:05 -0700

    Update lib version

--- lib/cadence/activity/task_processor.rb
+++ lib/cadence/activity/task_processor.rb
@@ -87,7 +87,7 @@ module Cadence
         # adding additional deserialization logic here to help read input that is passed from go-client
         # https://github.com/uber-go/cadence-client/blob/0.18.x/internal/encoding.go#L45-L58
         #
-        # this ruby client serializes / deserializes everything as one big string like blow:
+        # this ruby client serializes / deserializes everything as one big string like below:
         # [1012474654, "second input"]
         #
         # while go client serializes input as separate input followed by line break
